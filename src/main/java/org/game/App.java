7c3442d0f6ec1959d6fd9acf7afd74644497897c
TipstR <96343885+TipstR@users.noreply.github.com>
2025-06-18 12:09:08 +0200

    Initial Commit

---
+++ src/main/java/org/game/App.java
@@ -0,0 +1,46 @@
+package org.game;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        try {
+            String css = "app.css";
+
+            scene = new Scene(loadFXML("menu"), 640, 480);
+            scene.getStylesheets().add(getClass().getResource(css).toExternalForm());
+            stage.setScene(scene);
+            stage.setTitle("Crafting Game");
+            stage.show();
+        } catch(NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
