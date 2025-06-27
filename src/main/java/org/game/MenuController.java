382d351b3e07ebd8d3a6cc90021a3c698f780124
TipstR <96343885+TipstR@users.noreply.github.com>
2025-06-27 16:49:14 +0200

    added documentation to MenuController.java

--- src/main/java/org/game/MenuController.java
+++ src/main/java/org/game/MenuController.java
@@ -1,11 +1,18 @@
 package org.game;
 
 import javafx.fxml.FXML;
-
 import java.io.IOException;
 
+/**
+ * Controller for the Menu-UI.
+ */
 public class MenuController {
 
+
+    /**
+     * Function for starting a new game. This is called by JavaFX.
+     * @throws IOException Can throw IOException.
+     */
     @FXML
     private void newGame() throws IOException {
         App.setRoot("game");
