fedbcb1b308be9e0ea6f67d9096441856957b44c
TipstR <96343885+TipstR@users.noreply.github.com>
2025-06-29 11:58:20 +0200

    Added Worker for GameController.java

---
+++ src/main/java/org/game/Worker.java
@@ -0,0 +1,50 @@
+package org.game;
+
+/**
+ * Class to create worker, to keep track of the threads in GameController.java.
+ */
+public class Worker {
+    /**
+     * Determines if the thread should keep working.
+     */
+    private boolean working;
+    /**
+     * What job the worker has. This is used to determine in which list this worker is.
+     */
+    private final String job;
+
+    /**
+     * Constructor of Worker.
+     *
+     * @param newJob What job the worker gets.
+     */
+    public Worker(final String newJob) {
+        this.working = false;
+        this.job = newJob;
+    }
+
+    /**
+     * Getter of working.
+     *
+     * @return Current value of working.
+     */
+    public boolean isWorking() {
+        return working;
+    }
+
+    /**
+     * Setter of working.
+     * @param newWorking New value of working.
+     */
+    public void setWorking(final boolean newWorking) {
+        this.working = newWorking;
+    }
+
+    /**
+     * Getter of job.
+     * @return Current value of job.
+     */
+    public String getJob() {
+        return job;
+    }
+}
